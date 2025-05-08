f73979ae09d2a22147b7dd373ca934df5043bc10
p3choco <62072811+p3choco@users.noreply.github.com>
2025-05-08 20:20:42 +0200

    changes for sonar 2

--- frontend/src/components/Koszyk.js
+++ frontend/src/components/Koszyk.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { useCart, useCartDispatch } from '../context/CartContext';
+import { useCart } from '../context/CartContext';
 
 function Koszyk() {
     const cart = useCart();
